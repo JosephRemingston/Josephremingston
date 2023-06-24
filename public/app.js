164651c9c0913bb15ec922a72fdfa7cf7c961a8f
ljrem <ljremi@gmail.com>
2023-06-24 19:51:17 +0530

    initial commit

--- public/app.js
+++ public/app.js
@@ -2,7 +2,6 @@ redirect();
 function redirect(){
     document.addEventListener("click" , (e) => {
         let btn_class = e.target.classList;
-        console.log(btn_class);
         for (let i = 0 ; i < btn_class.length ; i++){
             if (btn_class[i] === "btn-portfolio-live"){
                 window.open("https://github.com/JosephRemingston/Joseph_website_" , "__blank");
