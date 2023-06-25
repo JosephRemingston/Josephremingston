b0d9c6c358f22c07a0db0211d9b903b713ed9ca0
ljrem <ljremi@gmail.com>
2023-06-25 23:57:06 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-//packages
-
 var express = require("express");
 var app = express();
 require("dotenv").config();
 var bodyParser = require("body-parser");
 var formData = require('form-data');
 var Mailgun = require('mailgun.js');
-
-// constant values
 var API_KEY = process.env.API_KEY;
 var DOMAIN = process.env.DOMAIN;
 var mailgun = new Mailgun(formData);
 var client = mailgun.client({username: 'api', key: API_KEY});
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/"));
-
-// main
 app.get("/" , (req , res) => {
   res.sendFile(__dirname + "/");
 })
-
 app.post('/', async (req, res) => {
   var messageData = {
     from: req.body.email,
     to: `ljremi@gmail.com`,
     subject: 'From form',
     text: req.body.message
-  };
+  }
   await client.messages.create(DOMAIN, messageData)
       .then((response) => {
         console.log("Mail sent");
@@ -37,7 +29,6 @@ app.post('/', async (req, res) => {
         console.error(err);
       });
 });
-
-app.listen(200 , () => {
+app.listen(process.env.PORT , () => {
   console.log("server");
 })
