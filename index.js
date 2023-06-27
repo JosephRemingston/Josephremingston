f46698fba1b2e32b19f5d8f33843dc99ae618764
ljrem <ljremi@gmail.com>
2023-06-27 20:15:37 +0530

    initial commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 var bodyParser = require("body-parser");
 var formData = require('form-data');
 var Mailgun = require('mailgun.js');
+var axios = require("axios");
 var API_KEY = process.env.API_KEY;
 var DOMAIN = process.env.DOMAIN;
 var mailgun = new Mailgun(formData);
@@ -14,21 +15,34 @@ app.use(express.static(__dirname + "/"));
 app.get("/" , (req , res) => {
   res.sendFile(__dirname + "/");
 })
-app.post('/', async (req, res) => {
+app.post('/', (req, res) => {
   var messageData = {
     from: req.body.email,
     to: `ljremi@gmail.com`,
     subject: 'From form',
     text: req.body.message
   }
-  await client.messages.create(DOMAIN, messageData)
+  axios.get("https://emailvalidation.abstractapi.com/v1/?api_key=" + process.env.MAIL_API + "&email=" + req.body.email)
+  .then (response => {
+    var ret = response.data;
+    var deliver = ret.deliverability;
+    if (deliver === "UNDELIVERABLE"){
+      res.send("check the email typed");
+    }
+    else if (deliver === "DELIVERABLE"){
+      client.messages.create(DOMAIN, messageData)
       .then((response) => {
         console.log("Mail sent");
       })
       .catch((err) => {
         console.error(err);
       });
-});
-app.listen(process.env.PORT , () => {
+    }
+  })
+  .catch(error => {
+      console.log(error);
+  })
+})
+app.listen(200 , () => {
   console.log("server");
 })
